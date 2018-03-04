e2ec48deb6963d820904aa5da72765db84c8f2c5
Hulavanne <pyry.liljamo@dnainternet.net>
2018-03-05 01:46:14 +0200

    Score saving added

--- Assets/Scripts/ProjectConstants.cs
+++ Assets/Scripts/ProjectConstants.cs
@@ -19,4 +19,30 @@ public static class ProjectConstants
 
         return new Vector2(_x, _y);
     }
+
+    public static string AddOrdinal(int number)
+    {
+        if (number <= 0)
+            return number.ToString();
+
+        switch (number % 100)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return number + "th";
+        }
+
+        switch (number % 10)
+        {
+            case 1:
+                return number + "st";
+            case 2:
+                return number + "nd";
+            case 3:
+                return number + "rd";
+            default:
+                return number + "th";
+        }
+    }
 }
--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,13 +13,19 @@ public class UserInterface : MonoBehaviour
     public RectTransform Overlay;
     public Text WaveText;
     public RectTransform GameOverScreen;
+    public RectTransform SubmitScoreScreen;
+    public RectTransform TopScoresScreen;
     public Text FinalScoreText;
+    public InputField NameInputField;
+    public RectTransform TopScores;
 
     private Image[] m_shipImages;
 
 	void Awake()
     {
         GameOverScreen.gameObject.SetActive(false);
+        TopScoresScreen.gameObject.SetActive(false);
+
         m_shipImages = RemainingShips.GetComponentsInChildren<Image>();
 	}
 
@@ -42,6 +50,19 @@ public class UserInterface : MonoBehaviour
         Overlay.gameObject.SetActive(false);
     }
 
+    public void ButtonClick_SubmitScore()
+    {
+        string _allScores = PlayerPrefs.GetString("Scores");
+        string _stringToSave = NameInputField.text + ":" + GameManager.Instance.Score + "|";
+        _allScores += _stringToSave;
+        PlayerPrefs.SetString("Scores", _allScores);
+
+        SetTopScoreTexts();
+
+        SubmitScoreScreen.gameObject.SetActive(false);
+        TopScoresScreen.gameObject.SetActive(true);
+    }
+
     public void ButtonClick_Restart()
     {
         GameManager.Instance.RestartGame();
@@ -60,4 +81,48 @@ public class UserInterface : MonoBehaviour
     public void ButtonUp_Down() { PlayerInput.DownButtonDown = false; }
 
     public void ButtonDown_Shoot() { PlayerInput.ShootInput = true; }
+
+    public void SetTopScoreTexts()
+    {
+        // Load all scores to a string, example of what it could look like:
+        // Name1:200|Name2:100|Name3:1000|
+        string _allScores = PlayerPrefs.GetString("Scores");
+
+        string _nameAndScore = "";
+        string _name = "";
+        int _score = 0;
+
+        Dictionary<string, int> _namesAndScores = new Dictionary<string, int>();
+
+        for (int i = 0; i < _allScores.Length; ++i)
+        {
+            if (_allScores[i] != '|')
+            {
+                _nameAndScore += _allScores[i];
+            }
+            else
+            {
+                string[] _splitStrings = _nameAndScore.Split(':');
+                _nameAndScore = "";
+
+                _name = _splitStrings[0];
+                _score = Convert.ToInt32(_splitStrings[1]);
+
+                _namesAndScores.Add(_name, _score);
+            }
+        }
+
+        _namesAndScores = _namesAndScores.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+        for (int i = 1; i < TopScores.transform.childCount; ++i)
+        {
+            if (i <= _namesAndScores.Count)
+            {
+                _name = _namesAndScores.Keys.ElementAt(i - 1);
+                _score = _namesAndScores.Values.ElementAt(i - 1);
+                TopScores.transform.GetChild(i).GetComponent<Text>().text = ProjectConstants.AddOrdinal(i) + ": " + _name + " | " + _score;
+            }
+            else TopScores.transform.GetChild(i).GetComponent<Text>().text = ProjectConstants.AddOrdinal(i) + ": ";
+        }
+    }
 }
